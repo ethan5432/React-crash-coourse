be458ccf552b6db38d5239e7a2a8bfa0e5c63773
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-23 09:39:07 -0400

    working on routing functions

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,61 +1,15 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/title.jsx";
-import Modal from "./components/Modal.jsx";
-import React, {
-  useState, useEffect
-} from "react";
-import Counter from "./components/Counter.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    console.log("onTodoDelete");
-    setShowModal(true);
-  }
-
-  function cancelModal() {
-    setShowModal(false);
-    console.log("cancel Modal");
-  }
-  function confirmlModal() {
-    setShowModal(false);
-    console.log("confirm Modal");
-  }
-
-  useEffect(() => {
-  console.log('on mount')
-  }, [])  
-  
-  useEffect(() => {
-  console.log(`on mount and on ${showModal} change`)
-}, [])  
-
   return (
     <div>
-      <Title />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
-        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
-        <Todo onTodoDelete={onTodoDelete} title="Get rich" />
-      </div>
-      {showModal && (
-        <Modal
-          cancelModal={cancelModal}
-          confirmModal={confirmlModal}
-          title="Are you sure?"
-        />
-      )}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- react-crash-course/src/Pages/Home.jsx
+++ react-crash-course/src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 function Home() {
-  return;
-  <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
