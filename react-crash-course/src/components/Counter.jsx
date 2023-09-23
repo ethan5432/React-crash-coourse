250d9b35ea0acbca93e4ecd231216e9dc4880f5c
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-23 11:18:19 -0400

    going to restart from command prompt

--- react-crash-course/src/components/Counter.jsx
+++ react-crash-course/src/components/Counter.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 
 
-function Counter() {
-const [arr, setArr] = useState([1])
+// function Counter() {
+// const [arr, setArr] = useState([1])
 
-  function addPlus() {
-        setArr(prevArr => [...prevArr, "+"])
-    }
+//   function addPlus() {
+//         setArr(prevArr => [...prevArr, "+"])
+//     }
     
-    function addMinus() {
-        setArr(prevArr => [...prevArr, "-"])
-    }
+//     function addMinus() {
+//         setArr(prevArr => [...prevArr, "-"])
+//     }
     
-    return (
-        <div>
-        <button onClick={addPlus}>+</button>
-        <button  onClick={addMinus}>-</button>
-        {arr}
-        </div>)
-}
+//     return (
+//         <div>
+//         <button onClick={addPlus}>+</button>
+//         <button  onClick={addMinus}>-</button>
+//         {arr}
+//         </div>)
+// }
 
- export default Counter
+//  export default Counter
--- react-crash-course/src/components/Modal.jsx
+++ react-crash-course/src/components/Modal.jsx
@@ -1,20 +1,20 @@
-import "./Modal.css"
+// import "./Modal.css"
 
-function Modal({ title, cancelModal, confirmModal }) {
+// function Modal({ title, cancelModal, confirmModal }) {
 
 
-    return (
-<>
-  <div className="modal">
-    <p className="modal__title">{title}</p>
-    <div className="modal__buttons">
-      <button className="btn btn__cancel" onClick={cancelModal}>Cancel</button>
-      <button className="btn" onClick={confirmModal}>Confirm</button>
-    </div>
-  </div>
-  <div className="backdrop" />
-</>
-    );
-}
+//     return (
+// <>
+//   <div className="modal">
+//     <p className="modal__title">{title}</p>
+//     <div className="modal__buttons">
+//       <button className="btn btn__cancel" onClick={cancelModal}>Cancel</button>
+//       <button className="btn" onClick={confirmModal}>Confirm</button>
+//     </div>
+//   </div>
+//   <div className="backdrop" />
+// </>
+//     );
+// }
 
-export default Modal;
+// export default Modal;
